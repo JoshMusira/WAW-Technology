770ce074953e84a8d42711a2fcba2be10c52cc17
Joshua Musira <joshuamusira01@gmail.com>
2026-09-18 17:17:03 +0300

    feat: update Dockerfile to install OpenSSL and modify start script to use tsx

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./server";
 
-const port = parseInt(process.env.PORT || "3000");
+const port = Number.parseInt(process.env.PORT || "3000");
 
 app.listen(port, () => {
     console.log(`Express is running on port ${port}`);
